464f74ef513918c63100a6742429b074e5750ddb
SHIMIZU Hisami <shimizu-hisami@fujielectric.co.jp>
2015-05-27 20:37:01 +0900

    tweak access-log setting.

--- src/main/java/com/fujielectric/ficks/config/LogConfig.java
+++ src/main/java/com/fujielectric/ficks/config/LogConfig.java
@@ -7,11 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.core.env.Environment;
 
+import javax.annotation.Resource;
 import java.net.URL;
+import java.util.Optional;
 
 @Configuration
 public class LogConfig {
+
+    static final String LOGBACK_ACCESS = "logback.access";
+
+    @Resource
+    private Environment environment;
 
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer(){
@@ -21,10 +29,16 @@ public class LogConfig {
 
                 if(factory instanceof TomcatEmbeddedServletContainerFactory){
                     TomcatEmbeddedServletContainerFactory containerFactory = (TomcatEmbeddedServletContainerFactory) factory;
+                    String logbackAccessConfig = environment.getRequiredProperty(LOGBACK_ACCESS);
 
-                    URL url = this.getClass().getResource("/logback-access.xml");
                     LogbackValve  logbackValve = new LogbackValve();
-                    logbackValve.setFilename(url.getFile());
+                    if (logbackAccessConfig == null) {
+                        URL url = this.getClass().getResource("/logback-access.xml");
+                        logbackValve.setFilename(url.getFile());
+                    } else {
+                        logbackValve.setFilename(logbackAccessConfig);
+                    }
+
                     containerFactory.addContextValves(logbackValve);
                 }
             }
